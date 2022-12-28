20fb68d4313bea208a4b4406cc820d8154f0ce83
ZoRoRooT <sudarshan_n@hotmail.com>
2022-12-28 22:26:54 +0530

    s

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -10,7 +10,7 @@ const {data,loading} = useFecth("/hotels/countByCity?cities=berlin,madrid,london
       {loading?("Loading Please wait"):(<><div className="featuredItem">
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-          alt=""
+          alt="img"
           className="featuredImg"
         />
         <div className="featuredTitles">
